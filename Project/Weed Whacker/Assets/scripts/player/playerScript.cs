d3bb812dab42ff8ddb75d6b9c5ce7ebe95240d1a
tama <slimey198700@gmail.com>
2024-03-04 12:35:57 -0800

    installed input manager and cinemachine; added player movement; added sunflower and weed prototype sprites

--- Project/Weed Whacker/Assets/scripts/player/playerScript.cs
+++ Project/Weed Whacker/Assets/scripts/player/playerScript.cs
@@ -1,19 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class playerScript : MonoBehaviour
 {
-    //public 
+    //references
+    public InputAction playerControls;
+    public Rigidbody2D myRigidBody2D;
+
+    //handling
+    public float moveSpeed;
+
+    //internal control variables
+    private class crntState;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
+    }
+
+    private void OnEnable()
+    {
+        playerControls.Enable();
+    }
+    private void OnDisable()
+    {
+        playerControls.Disable();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        myRigidBody2D.velocity = new Vector2(playerControls.ReadValue<Vector2>().x * moveSpeed, playerControls.ReadValue<Vector2>().y * moveSpeed);
+    }
+
+    private void stateTransition()
+    {
+
     }
 }
