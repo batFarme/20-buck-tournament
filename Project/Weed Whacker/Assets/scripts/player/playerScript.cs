31f226776b79dcb63b36c13bda33400db5d78a75
tama <slimey198700@gmail.com>
2024-03-05 13:33:52 -0800

    C# is so fuckd broooooo

--- Project/Weed Whacker/Assets/scripts/player/playerScript.cs
+++ Project/Weed Whacker/Assets/scripts/player/playerScript.cs
@@ -48,14 +48,14 @@ public class playerScript : MonoBehaviour
     {
         crntState();
     }
-
+    /*
     private void stateTransition(string stateName)
         //rather than simply setting the crntState var in code directly, use this (future-proofing)
         //also WAAAHHH IDK WHAT VARIABLE TYPE FUNCTIONS AREEEEEE this is so effed up man
     {
 
     }
-
+    */
     // states; when set as crntState, they are called every frame
     private void standingState() //stateName is standingState
     {
