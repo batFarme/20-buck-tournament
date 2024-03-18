ecf4a40c8f9d0200052eda908656a8a40012e6c1
tama <slimey198700@gmail.com>
2024-03-18 13:34:30 -0700

    a little srping cleaning

--- Project/Weed Whacker/Assets/scripts/player/knockedState.cs
+++ Project/Weed Whacker/Assets/scripts/player/knockedState.cs
@@ -13,12 +13,12 @@ public class knockedState : BaseState<playerScript.PlayerStates> // this gives a
     
     public override void EnterState()
     {
-        throw new System.NotImplementedException();
+        
     }
 
     public override void ExitState()
     {
-        throw new System.NotImplementedException();
+        
     }
 
     public override playerScript.PlayerStates GetNextState()
@@ -28,33 +28,22 @@ public class knockedState : BaseState<playerScript.PlayerStates> // this gives a
 
     public override void OnTriggerEnter(Collider2D collision)
     {
-        throw new System.NotImplementedException();
+        
     }
 
     public override void OnTriggerExit(Collider2D collision)
     {
-        throw new System.NotImplementedException();
+        
     }
 
     public override void OnTriggerStay(Collider2D collision)
     {
-        throw new System.NotImplementedException();
+        
     }
 
     public override void UpdateState()
     {
-        throw new System.NotImplementedException();
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
