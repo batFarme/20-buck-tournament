73e0e5513ae66cbce8af6e6721ac3d3d7bdd783d
tama <slimey198700@gmail.com>
2024-05-01 13:42:00 -0700

    need to learn regular c# events....

--- Project/Weed Whacker/Assets/scripts/player guardian angel/guardianAngelJoinBehavior.cs
+++ Project/Weed Whacker/Assets/scripts/player guardian angel/guardianAngelJoinBehavior.cs
@@ -6,12 +6,13 @@ using UnityEngine.InputSystem;
 
 public class guardianAngelJoinBehavior : MonoBehaviour
 {
-    public UnityEvent joinButtonSmacked;
+    public Event joinButtonSmacked;
+    public int flagVar = 1;
     //this script will rely on playerGuardianAngel.cs 's references instead of making its own, in an effort to cut down on memory usage. 
     public void spawnPlayer(InputAction.CallbackContext context)
     {
         print("join action smacked!");
-        joinButtonSmacked.Invoke();
+        //joinButtonSmacked.Invoke();
         var playerSpawn = Instantiate(this.gameObject.GetComponent<playerGuardianAngelScript>().playerPrefab);
         playerSpawn.GetComponent<playerScript>().controls = this.gameObject.GetComponent<playerGuardianAngelScript>().playerControlssss;
         playerSpawn.GetComponent<playerScript>().okayNOWYouCanStart();
--- Project/Weed Whacker/Assets/scripts/videoPlayerScript.cs
+++ Project/Weed Whacker/Assets/scripts/videoPlayerScript.cs
@@ -13,14 +13,17 @@ public class videoPlayerScript : MonoBehaviour
         print("it did the thing");
         GameObject player1 = GameObject.Find("player 1 guardian angel");
         player1.AddComponent<guardianAngelJoinBehavior>();
-        player1.GetComponent<guardianAngelJoinBehavior>().joinButtonSmacked.AddListener(itsShowtime);
+        print(player1.GetComponent<guardianAngelJoinBehavior>().flagVar);
+        //player1.GetComponent<guardianAngelJoinBehavior>().joinButtonSmacked.AddListener(itsShowtime);
+        //player1.GetComponent<guardianAngelJoinBehavior>().joinButtonSmacked += itsShowtime;
         GameObject player2 = GameObject.Find("player 2 guardian angel");
         player2.AddComponent<guardianAngelJoinBehavior>();
-        player2.GetComponent<guardianAngelJoinBehavior>().joinButtonSmacked.AddListener(itsShowtime);
+        //player2.GetComponent<guardianAngelJoinBehavior>().joinButtonSmacked.AddListener(itsShowtime);
     }
 
     public void itsShowtime()
     {
-        SceneManager.LoadScene("demoScreen");
+        print("its showtime!!!!");
+        SceneManager.LoadScene("debugScene");
     }
 }
